ca94dfe2b6ee30b1b19aa663d251a7b07ca8910b
Dorian Weber <dorianweb57@gmail.com>
2026-02-23 14:26:57 +0100

    feat(pokeapi): création de la fonction pour récupérer les url

---
+++ modules/pokeapi.js
@@ -0,0 +1,14 @@
+function getASprite (spriteVoulu) {
+    fetch("http://127.0.0.1:8080/data/pokemon.json")
+        .then(response => {
+            return response.json()
+        })
+        .then(function(data) {
+            data.array.forEach(element => { 
+                if (element.name === spriteVoulu) {
+                    return fetch(element.url)
+                } else { throw new Error}
+            })
+        });
+
+}
